83a5c0b7598eb07d6fc4f006a07d60ad4db5a6b7
Vitalii Shylienkov <vshilenkov@hotmail.com>
2021-10-23 18:15:04 +0200

    clang-format: introduced
    - clang-format: base test for sort includes directive

---
+++ clang-format-test/sort.includes.hpp
@@ -0,0 +1,14 @@
+#include "quotes.hpp"
+#include "quotes.h"
+#include <angles/paths>
+#include <angles>
+#include <angles.hpp>
+#include <angles/paths.hpp>
+#include <angles.h>
+#include <angles/paths.h>
+#include "quotes/paths.h"
+#include "quotes/paths.hpp"
+
+
+
+
